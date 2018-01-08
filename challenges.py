e9836890ffd253e1601b42f1b050c8a71b7856fd
Sadie Staudacher <sadiestaudacher@Sadies-MacBook-Pro.local>
2018-01-08 11:42:56 -0500

    3.2

--- challenges.py
+++ challenges.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import requests
 
 app = Flask(__name__)
 app.debug = True
@@ -22,9 +23,9 @@ def enterData():
     s = """<!DOCTYPE html>
 <html>
 <body>
-<form>
+<form action="http://localhost:5000/result" method="POST">
   INGREDIENT:<br>
-  <input type="text" name="ingredient" value="eggs">
+  <input type="text" name="ingredient">
   <br>
   <input type="submit" value="Submit">
 </form>
@@ -39,14 +40,43 @@ def enterData():
 ## to display recipes for the ingredient entered
 @app.route('/result',methods = ['POST', 'GET'])
 def displayData():
+    baseurl = "http://www.recipepuppy.com/api"
     if request.method == 'POST':
-        pass
+      ingredient = request.form['ingredient']
+      param_dict = {'i':ingredient}
+      req = requests.get(baseurl, params = param_dict) #response object
+      diction = req.json()
+      list_of_recipies = []
+      for recipe in diction['results']:
+        list_of_recipies.append(recipe['title'])
+      return str(list_of_recipies)
+
+    if request.method == 'GET':
+      ingredient = request.args['ingredient']
+      param_dict = {'i':ingredient}
+      req = requests.get(baseurl, params = param_dict) #response object
+      diction = req.json()
+      list_of_recipies = []
+      for recipe in diction['results']:
+        list_of_recipies.append(recipe['title'])
+      return str(list_of_recipies)
+
+
+
+
+      
+      
+
+
+
+      
+
 
 ## Task 4
 ## Note : Since this is a dyanmic URL, recipes function should recieve a paramter called `ingrdient` 
 @app.route('/recipe/<ingredient>')
 def recipes():
-    pass
+  pass
 
 if __name__ == '__main__':
     app.run()
